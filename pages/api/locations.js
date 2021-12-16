df797bbc757b5e2ec1062444743c5743d25ddf18
Abdul Zahid <awahab07@yahoo.com>
2021-12-16 13:18:00 +0100

    Only json locations as public asset

--- pages/api/locations.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-export default function (req, res) {
-    const json = fs.readFileSync(path.resolve('../../data/json/locations.json'), 'utf8');
-    return res.status(200).json(json);
-}
